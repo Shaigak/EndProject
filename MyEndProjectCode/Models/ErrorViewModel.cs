ebfff8b90897c13db5e3b3cf2e44f13e8798d73c
[shaigak] <shaigak@code.edu.az>
2023-06-03 00:35:37 +0300

    new

---
+++ MyEndProjectCode/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyEndProjectCode.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
