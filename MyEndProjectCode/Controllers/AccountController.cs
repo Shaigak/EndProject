939bd94156fdb396927f3efd74a9d20a8840c454
[shaigak] <shaigak@code.edu.az>
2023-06-11 15:25:39 +0300

    new

---
+++ MyEndProjectCode/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyEndProjectCode.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+    }
+}
